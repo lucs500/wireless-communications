6dd01c5827e396ce980583e8c30a56c5a7225d07
lucs500 <lucasbezrocha@gmail.com>
2023-12-08 23:10:28 -0300

    Update logdistance-Pr.py

--- logdistance-Pr.py
+++ logdistance-Pr.py
@@ -2,11 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f(x):
-return -35.547-32.3*np.log(x) #equação F(n)
+   return -35.930-66.8*np.log10(x) #equação F(n)
 
-x = np.linspace(0, 1000)
+x = np.linspace(1, 10)
 
-plt.plot(x, f(x), color='red')
+y = [1, 2, 3, 4, 5, 10]
+z = [-35.93, -69.99, -70.43, -73.39, -86.2, -95.82] #pontos medidos
+
+plt.scatter(y, z, label='Pontos medidos')
+
+plt.plot(x, f(x), color='red', label='Função encontrada')
 plt.legend()
 plt.xlabel('d (m)')
 plt.ylabel('Pr (dBm)')
